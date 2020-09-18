498470c3907ebb4db21a845b57dd73f5beb6d42f
JayPTucker <jayptuckerimportant@gmail.com>
2020-09-18 16:21:52 -0500

    All inital files installed

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+
+function Navbar() {
+    return (
+        <Row>
+            <Col md={12}>
+                <p>test</p>
+            </Col>
+        </Row>
+    )
+}
+
+export default Navbar;
